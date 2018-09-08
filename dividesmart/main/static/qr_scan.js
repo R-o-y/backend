a6ed21bc682ce72f6668cf13be0951e20f5e2ec7
royl8 <royl8@qq.com>
2018-09-08 21:05:28 +0800

    no message

--- dividesmart/main/static/qr_scan.js
+++ dividesmart/main/static/qr_scan.js
@@ -14,16 +14,17 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      delay: 300,
+      delay: 80,
       result: 'No result',
     }
     this.handleScan = this.handleScan.bind(this)
   }
   handleScan(data){
-    if(data){
+    if (data){
       this.setState({
         result: data,
       })
+      window.location.href = data
     }
   }
   handleError(err){
